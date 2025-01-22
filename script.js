94fd4f9050ecaf1f56d94163d2338dd75830790a
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2025-01-22 11:25:50 +0100

    lek9_upg2

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 
-/* let tal1 = parseInt();
-let tal2 = parseInt();
-let svar = parseInt();
- */
+
+let lista_history = document.querySelector("#history");
+
 const tal1_input = document.querySelector("#input_number_1");
 const tal2_input = document.querySelector("#input_number_2");
-const output = document.querySelector("#output_number");
+const svar = document.querySelector("#output_number");
 
-function Addera(){
-   const tal1 = parseInt(tal1_input.value);
-   const tal2 = parseInt(tal2_input.value);
+function additionClick() {
+    const tal1 = Number(tal1_input.value);
+    const tal2 = Number(tal2_input.value);
+    svar.value = tal1 + tal2;
 
-    output.value = tal1 + tal2;
-   
+    let li_history = document.createElement("li");
+    li_history.textContent = tal1 + " + " + tal2 + " = " + svar.value;
+    lista_history.appendChild(li_history);
 }
 
-function Subtrahera(){
-    const tal1 = parseInt(tal1_input.value);
-    const tal2 = parseInt(tal2_input.value);
- 
-     output.value = tal1 - tal2;
+function subtractionClick() {
+    const tal1 = Number(tal1_input.value);
+    const tal2 = Number(tal2_input.value);
+    svar.value = tal1 - tal2;
     
- }
+    let li_history = document.createElement("li");
+    li_history.textContent = tal1 + " + " + tal2 + " = " + svar.value;
+    lista_history.appendChild(li_history);
+}
 
- function Multiplicera(){
-    const tal1 = parseInt(tal1_input.value);
-    const tal2 = parseInt(tal2_input.value);
- 
-     output.value = tal1 * tal2;
+function multiplicationClick() {
+    const tal1 = Number(tal1_input.value);
+    const tal2 = Number(tal2_input.value);
+    svar.value = tal1 * tal2;
     
- }
+    let li_history = document.createElement("li");
+    li_history.textContent = tal1 + " * " + tal2 + " = " + svar.value;
+    lista_history.appendChild(li_history);
+}
 
- function Dividera(){
-    const tal1 = parseInt(tal1_input.value);
-    const tal2 = parseInt(tal2_input.value);
- 
-     output.value = tal1 / tal2;
+function divisionClick() {
+    const tal1 = Number(tal1_input.value);
+    const tal2 = Number(tal2_input.value);
+    svar.value = tal1 / tal2;
     
- }
+    let li_history = document.createElement("li");
+    li_history.textContent = tal1 + " ÷ " + tal2 + " = " + svar.value;
+    lista_history.appendChild(li_history);
+}
